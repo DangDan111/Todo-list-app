4d859f0a6fce21672464946711cbc3f02c62352a
DangDan111 <dangdan0404@gmail.com>
2025-10-21 09:58:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def add_task(task_name):
     tasks.append(task_name)
     print(f"Đã thêm công việc mới: '{task_name}'")
 #---Điểm bắt đầu của chương trình ---
-if __name__=="__main___":
+if __name__=="__main__":
     print("Chào mừng đến với ứng dụng To-Do List!")
     add_task("Học bài với Git và GitHub")
     add_task("Làm bài tập thực hành ở nhà")
